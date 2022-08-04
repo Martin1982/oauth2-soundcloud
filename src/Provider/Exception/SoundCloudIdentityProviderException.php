769541a414b152def9a58a0d25c8c641b7c29543
Martin de Keijzer <martin@spinninrecords.nl>
2022-08-04 14:24:35 +0200

    Setup basic provider

---
+++ src/Provider/Exception/SoundCloudIdentityProviderException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Martin1982\OAuth2\Client\Provider\Exception;
+
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+
+class SoundCloudIdentityProviderException extends IdentityProviderException
+{
+}
---
+++ src/Provider/Exception/SoundCloudProviderException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Martin1982\OAuth2\Client\Provider\Exception;
+
+class SoundCloudProviderException extends \Exception
+{
+}
